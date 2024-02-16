68c8c70588616a344e096c8c17e63a82eda2754c
NikitDudinov <dudinovnik@yandex.ru>
2024-02-16 21:37:02 +0300

    Cerate repositoty

---
+++ Lw_1/about_me.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Никита Дудинов</p>
+    <?php
+    $years = "2005";
+    $month = "01";
+    $date = "06";
+    if($month > date('m') || $month == date('m') && $date > date('d'))
+      $age = date('Y') - $years - 1; 
+    else
+      $age = date('Y') - $years;
+     echo $age, ' лет';
+    ?>
+    <p>Мои интересы:</p>   
+    <ul>
+        <?php 
+        $interests= array('Футбол','Автомобили', 'Мотоциклы', 'Программирование', 'Музыка');
+        foreach($interests as $item): 
+            $str = htmlentities($item, ENT_QUOTES);
+            echo "<li>$str</li>";
+        endforeach; 
+        ?>    
+    </ul>
+</body>
+</html>
